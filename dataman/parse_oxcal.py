fea82b52b3172d2de4c10c5b7eb5f432ecb4b83c
Geoscience Australia <jono.d.griffin@gmail.com>
2019-01-07 21:48:18 +1300

    Initial code to parse oxcal output

--- dataman/parse_oxcal.py
+++ dataman/parse_oxcal.py
@@ -8,8 +8,6 @@ import os, sys
 import csv
 from QuakeRates.dataman.event_dates import EventDate
 
-key_dict = {'I': ['Calculate', 'posterior']
-
 def parse_oxcal(filename, keys):
     """Parse in csv output file from OxCal and extract data as desired.
     :param filename: string of path to input file
@@ -18,7 +16,7 @@ def parse_oxcal(filename, keys):
     raw C14 dates
     """
 
-    with open(filename) as csvfile:
+    with open(filename) as csv_file:
         csv_reader = csv.DictReader(csv_file)
         line_count = 0
         for row in csv_reader:
@@ -26,8 +24,29 @@ def parse_oxcal(filename, keys):
                 print(f'Column names are {", ".join(row)}')
                 line_count += 1
             else:
-                if csv_reader['name'] in key_dict:
-                    
+                if row['name'] in key_dict:
+                    print(row['op'])
+                    print(row['type'])
+                    print(key_dict[row['name']][0])
+                    print(key_dict[row['name']][1])
+                    if row['op'] ==  key_dict[row['name']][0] and \
+                       row['type'] == key_dict[row['name']][1]:
+                        date_dict[row['name']].append(row['value'])
+                        prob_dict[row['name']].append(row['probability'])
+    print(date_dict['I'])
+    print(prob_dict['I'])
+
+
+if __name__ == "__main__":
+    filename = '../data/Xorkoli_Altyn_Tagh_Yuan_2018.csv'
+    key_dict = {'I': ['Calculate', 'posterior'],
+                'H': ['Calculate', 'posterior']} 
+    date_dict = {}                                                                                 
+    prob_dict = {}
+    for key in key_dict:
+        date_dict[key] = []
+        prob_dict[key] = []
+    parse_oxcal(filename, key_dict)
     
     
     
