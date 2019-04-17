f1aa252d7df6518d4d40fbe1427f78bfe755554f
Geoscience Australia <jono.d.griffin@gmail.com>
2019-04-17 13:07:47 +1200

    Testing different priors

--- R/models/fit_exp_data_ksamples.R
+++ R/models/fit_exp_data_ksamples.R
@@ -4,7 +4,7 @@
 library(R2jags)
 library(lattice)
 # Fix random seed
-set.seed(23)
+#set.seed(23)
 
 setwd('.')
 
@@ -12,15 +12,17 @@ setwd('.')
 ###########
 # Real data
 
-#datafile = '/Users/PCUser/Documents/PhD/modelling/QuakeRates/dataman/chronologies10000.csv'
+#datafile = '/Users/PCUser/Documents/PhD/modelling/QuakeRates/dataman/chronologies100.csv'
+#datafile = 'chronologies100.csv'
 #data = read.csv(datafile, header=FALSE)#, delimiter=',')
 
-#k=10000
+#k=100
 
 # Convert data to inter-event times
 #m = data.matrix(data)
 #inter_event_m = t(diff(t(m))) # Transpose, take difference and transpose back
 #Y = inter_event_m
+#print(Y)
 
 ##############
 # Test data
@@ -28,28 +30,38 @@ n.sim <- 100
 lam <- 1./100 # Define as inverse of rate
 mean_time <- 1./lam
 y_init <- rexp(n=n.sim, rate=lam) # Exponential DV
-k = 2 #2 # 10
-y = cbind(y_init, y_init) # if k=2
-#y = cbind(y_init, y_init, y_init, y_init, y_init, y_init, y_init, y_init, y_init, y_init) # k=10
+k = 10 #2 # 10
+##y = cbind(y_init, y_init) # if k=2
+y = cbind(y_init, y_init, y_init, y_init, y_init, y_init, y_init, y_init, y_init, y_init) # k=10
 y = data.matrix(y)
-print(y)
+##print(y)
 print(t(y))
 Y = t(y)
+#################
+N <- length(Y[1,])
+print(N)
+print(Y[1,1])
+print(Y[1,2])
+print(mean(Y[1,]))
+print(mean(Y[2,]))
+print(mean(Y[3,]))
+print(mean(Y[4,]))
+print(mean(Y))
 ###############
 
 sim.data.jags <- list("Y", "N", "k")
 
 # Define the parameters whose posterior distributions we want to calculate
-bayes.mod.params <- c("mu", "lambda", "lambda_k")
+bayes.mod.params <- c( "mu", "muk", "sigma")
 
 #Define starting values
 bayes.mod.inits <- function(){
-		list("lambda"=1/500.)
+		list("mu"=1/0.01)
 			     }
 
 bayes.mod.fit <- jags(data = sim.data.jags, #inits = bayes.mod.inits,
 	      parameters.to.save = bayes.mod.params, n.chains = 3,
-	      n.iter = 9000, n.burnin = 1000, model.file = 'exp_ksamples.bug')
+	      n.iter = 9000, n.burnin = 1000, model.file = 'exp_ksamples.jags')
 
 print(bayes.mod.fit)
 plot(bayes.mod.fit)
