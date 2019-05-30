25e0a4ac9201442854103f6e0dc58ac2d7c74a9a
Jonathan Griffin <griffij@ou094187.otago.ac.nz>
2019-05-31 09:25:09 +1200

    Limiting x-axis range

--- plotting/plot_otago_time_space.py
+++ plotting/plot_otago_time_space.py
@@ -2,15 +2,17 @@
 """
 #import matplotlib
 from matplotlib import pyplot as plt
+import matplotlib.patches as patches
 import numpy as np
 
 Akatore = [[-1,13]]
 Ak_n = ['3']
-Titri = [[17.5, 18.5], [71, 128]]
+#Titri = [[17.5, 18.5], [71, 128]] # real data
+Titri = [[17.5, 18.5], [71, 80]] # Dummy data for nice plotting
 T_n = ['?', '> 1']
 Dunstan = [[15,24], [54,56]]
 D_n = ['4 - 6', '?']
-Pisa_L = [[100,100]] # Dummy data for plotting text                                                                               
+Pisa_L = [[25,25]] # Dummy data for plotting text                                                                               
 P_L_n = ['No late Quaternary deformation']    
 Pisa_GV = [[35,70],[200,250]]
 P_GV_n = ['> 20?', '?']
@@ -28,6 +30,7 @@ faults = [Akatore, Titri, Dunstan, Pisa_L, Pisa_GV, Cardrona_BK, Cardrona_Kawara
           Little_Nevis]
 event_numbers = [Ak_n, T_n, D_n, P_L_n, P_GV_n, C_BK_n, C_K_n, N_BN_n, N_CC_n, LN_n]
 
+#plt.xlim([-5, 50])
 for i, fault in enumerate(faults):
     for j, date_range in enumerate(fault):
         plt.plot(date_range, [i,i], c='k', linewidth=1)
@@ -36,7 +39,7 @@ for i, fault in enumerate(faults):
 plt.plot([125,150],[0,0], c='k', linewidth=1, linestyle=':')
 plt.text(124, 0.1, '? > 125 ka')
 plt.ylim([-0.2, len(faults)-0.7])
-#plt.xlim([0, 150]) 
+#plt.xlim([-5, 50]) 
 plt.yticks(np.arange(0, len(faults)+1, 1),
            ['Akatore', 'Titri', 'Dunstan', 'Pisa-\nLindis', 'Pisa-\nGrandview', 'NW Cardrona\n(Branch Ck)',
             'NW Cardrona\n(Kawarau Gorge)', 'Nevis\n(Ben Nevis)', 'Nevis\n(Coal Creek)',
@@ -47,6 +50,23 @@ plt.annotate('', xy=(1.05, 0), xycoords='axes fraction', xytext=(1.05, 1),
             arrowprops=dict(arrowstyle="<-", color='k'))
 plt.text(0.95, 0.7, 'Proximity to Alpine Fault', transform=plt.gcf().transFigure,
          rotation=-90)
+#plt.plot([80,80],[-1,len(faults)+1], linestyle=':', linewidth=0.2)   
 plt.tight_layout()
 plt.savefig('Otago_time_space.png')
         
+plt.xlim([-5, 100])
+plt.xticks(np.arange(0, 90, 10))
+# Create a Rectangle patch
+rect = patches.Rectangle((-5,4.5),85,5.5,linewidth=0.5,edgecolor='k',facecolor='none',
+                         linestyle='-.')
+# Add the patch to the Axes
+ax = plt.gca()
+ax.add_patch(rect)
+#plt.text(30, 3.1, 'No late Quaternary deformation')
+plt.text(81, 0.1, '? > 125 ka')
+plt.text(81, 1.1, '? < 128 ka')
+plt.text(81, 4.1, '? ~ 250 ka')
+plt.text(40,8.0, 'Nevis-Cardrona\nFault System')
+plt.plot([80,80],[-1,len(faults)+1], linestyle=':', linewidth=1, c='0.5')
+plt.savefig('Otago_time_space_80ka.png') 
+         
