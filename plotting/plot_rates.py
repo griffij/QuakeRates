a5d3399d1760a3ef728a26362290cf19a9a0d807
Jonathan Griffin <jono.d.griffin@gmail.com>
2019-11-21 16:02:14 +1300

    Adding Hyde

--- plotting/plot_rates.py
+++ plotting/plot_rates.py
@@ -9,6 +9,7 @@ from matplotlib import pyplot as plt
 akatore = [115000, 600, 4, 125000, 'Akatore']
 cadell = [1500000, 7500, 8, 4500000,'Cadell']
 dunstan = [30000, 2000, 7, 55000, 'Dunstan']
+hyde = [25000, 10000, 2, 52000, 'Hyde'] # Just estimated for now
 lake_george = [750000, 3000, 30, 3000000, 'Lake George']
 lake_edgar = [1000000, 16500, 5, 2000000, 'Lake Edgar']
 hyden = [140000, 30000, 3, 250000, 'Hyden']
@@ -26,7 +27,7 @@ san_jacinto_anza = [254, 254, 15, 4000, 'San Jacinto'] # Rockwell et al 2015
 
 meers = [120000, 1700, 2, 120000, 'Meers']
 
-data = np.array([akatore, cadell, dunstan, lake_edgar, hyden,
+data = np.array([akatore, cadell, dunstan, hyde, lake_edgar, hyden,
                  galeen, roer, wharekuri, lake_george,
                  wairau_cloudy_bay, wairau_full_rupture,
                  alpine, san_jacinto_anza,
@@ -146,21 +147,24 @@ plt.savefig('event_per_year_vs_active_period.png')
 
 x = data[:,1]/data[:,0]
 y = data[:,2]/data[:,3]                                                                                                                                   
-print(x)                                                                                                                                                
-print(y)                                                                                                                                                 
-plt.clf()                                                                                                                                                
-plt.scatter(x,y)                                                                                                                                       
-ax = plt.gca()                                                                                                                                         
-ax.set_xscale('log')                                                                                                                                     
-ax.set_yscale('log')                                                                                                                                     
-ax.set_ylim([0.9*min(y), 1.1*max(y)])                                                                                                                    
+print(x)
+print(y)
+plt.clf()
+plt.scatter(x,y)
+ax = plt.gca()
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_ylim([0.9*min(y), 1.1*max(y)])                                                                                                    
 ax.set_xlim([0.9*min(x), 1.1*max(x)]) 
 ax.set_xlabel('Ratio Active:Quiescent', fontsize=18)                                                                                                  
 ax.set_ylabel('Events per year', fontsize=18)                                                                                                             
 texts = []
-for i, label in enumerate(labels):                                                                                                                     
-    texts.append(plt.text(x[i], y[i], label))                                                                                                        
-142#    ax.annotate(label, (x[i], y[i]))                                                                                                                 
+for i, label in enumerate(labels):
+    if label == 'Akatore' or label == 'Dunstan' or label == 'Hyde':         
+        texts.append(plt.text(x[i], y[i], label, color='r'))
+    else:
+        texts.append(plt.text(x[i], y[i], label, color='k'))
+#    ax.annotate(label, (x[i], y[i]))                                                                                                                 
 adjust_text(texts)#, arrowprops=dict(arrowstyle="->", color='r', lw=0.5))
 plt.tight_layout()                                                                                                                                     
 plt.savefig('event_per_year_vs_ratio_active_quiescent.png') 
