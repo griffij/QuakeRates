bb86ebb3cf93ac53dd91245d12917959d9435c87
Geoscience Australia <jono.d.griffin@gmail.com>
2020-03-27 10:22:19 +1300

    Moving exponential fit code into a callable function

---
+++ R/models/fit_exp_ksamples.R
@@ -0,0 +1,36 @@
+# Function for creating an R2jags model for an exponential distribution
+# where we have k samples of that distribution.
+# Y = data
+# N = number of interevent_times
+# k number of samples (e.g. considering k samples of earthquake chronologies
+#   from the output of an OxCal model
+
+library(R2jags)
+library(lattice)
+
+fit_exp_k_samples <- function(Y, N, k){
+	library(lattice)
+	library(R2jags)	
+	sim.data.jags <- list("Y", "N", "k")
+	
+	# Define the parameters whose posterior distributions we want to calculate
+	bayes.mod.params <- c( "mu")#, "lambda_k") #Don't plot out many lambdas
+
+	#Define starting values
+	bayes.mod.inits <- function(){
+			list("mu"=1/0.01)
+			}
+
+	bayes.mod.fit <- jags(data = sim.data.jags, #inits = bayes.mod.inits,
+		            parameters.to.save = bayes.mod.params, n.chains = 3,
+			    n.iter = 9000, n.burnin = 1000, model.file = 'exp_ksamples.jags')
+
+	print(bayes.mod.fit)
+	plot(bayes.mod.fit)
+	traceplot(bayes.mod.fit)
+
+	# Convert to an MCMC object
+	bayes.mod.fit.mcmc <- as.mcmc(bayes.mod.fit)
+	summary(bayes.mod.fit.mcmc)
+	return(bayes.mod.fit.mcmc)
+	}
