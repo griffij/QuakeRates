1e7a55fab4694d0362646a750eb1e67936c2e188
Geoscience Australia <jono.d.griffin@gmail.com>
2020-10-26 09:31:21 +1300

    Plot tweaks

--- plotting/plot_pdfs.py
+++ plotting/plot_pdfs.py
@@ -6,21 +6,21 @@ import numpy as np
 from scipy.stats import expon, gamma, weibull_min, lognorm
 
 
-scale = 500
-exp = expon(scale=scale)
+mean = 500
+exp = expon(scale=mean)
 
-gam = gamma(2.2, scale=scale)
+gam = gamma(2.2, scale=500/2.2)
 
 x = np.arange(0.01, 3000, 1)
 exp_pdf = exp.pdf(x) 
 plt.plot(x, exp_pdf, 'k-', lw=2, label=r'Exponential $(\theta = 500)$')
-plt.plot(x, gam.pdf(x), 'b-', lw=2, label=r'Gamma $(\alpha = 2.2, \theta = 500)$')
+plt.plot(x, gam.pdf(x), 'b-', lw=2, label=r'Gamma $(\alpha = 2.2, \theta = 500/\alpha)$')
 plt.legend()
 plt.xlabel('Inter-event time (Years)')
 plt.ylabel('Probability density')
 # Add mean
 plt.xlim([0, 3000])
 plt.ylim([0, max(exp_pdf)]) 
-plt.plot([scale, scale], [0, max(exp_pdf)], c='0.5', linestyle='--')
+plt.plot([mean, mean], [0, max(exp_pdf)], c='0.5', linestyle='--')
 plt.tight_layout()
 plt.savefig('pdf_examples.png')
