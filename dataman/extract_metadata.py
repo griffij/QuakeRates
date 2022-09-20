a2a46f40816d82509c9decb3ee2ff93f3a1e6e45
Geoscience Australia <jono.d.griffin@gmail.com>
2022-09-20 13:53:19 +1000

    Additional metadata extraction

--- dataman/extract_metadata.py
+++ dataman/extract_metadata.py
@@ -4,6 +4,7 @@
 from QuakeRates.dataman.parse_params import parse_param_file, get_event_sets
 from glob import glob
 import csv
+import numpy as np
 
 paramfiles = glob('../params/*.txt')
 # Remove unwanted records      
@@ -11,12 +12,18 @@ paramfiles.remove('../params/EmersonNorth_Rockwell_2000_simple.txt')
 
 f_out = open('metadata_summary.csv', 'w')
 writer = csv.writer(f_out)
-header = ['Name', 'Tectonic_region', 'Faulting_style']
+header = ['Name', 'Tectonic_region', 'Faulting_style', 'Method', 'Longitude',
+          'Latitude', 'Location_accuracy']
 names, event_sets, event_certainties, num_events, \
-    tect_regions, fault_styles = get_event_sets(paramfiles, ['all'], ['all'], 1)
-for i, event_set in enumerate(event_sets):
-    if num_events[i] >= 5:
-        metadata = [names[i], tect_regions[i], fault_styles[i]]
+    tect_regions, fault_styles, methods, \
+    longitudes, latitudes, location_accuracies = get_event_sets(paramfiles, ['all'], ['all'], 1,
+                                                return_full=True)
+sort_index = np.argsort(names)
+for i in sort_index:
+#for i, event_set in enumerate(event_sets):
+    if num_events[i] >= 5:# and longitudes[i] != 'NULL':
+        metadata = [names[i], tect_regions[i], fault_styles[i], methods[i],
+                    longitudes[i], latitudes[i], location_accuracies[i]]
         writer.writerow(metadata)
 
 f_out.close()
